a98ccdad4e416c553f206653f7d58c48af77c862
InvidentXp <invidentssc@hotmail.com>
2021-06-07 02:16:17 +0800

    Update fsm_binaryfile.go

--- fsm_binaryfile.go
+++ fsm_binaryfile.go
@@ -8,48 +8,84 @@ import (
 	"fmt"
 	"io"
 	"math"
+	"os"
 )
 
-func (f *fileHashmap) writeUInt32(pos int64, val uint32, filePos uint) {
-	if f.fileList[filePos] == nil {
-		panic(fmt.Errorf("file %d not open", filePos))
+func (f *fileHashmap) readByte(absPos int64, size uint64, file *os.File) []byte {
+	if file == nil {
+		panic(fmt.Errorf("file not open"))
 	}
 
-	var buf bytes.Buffer
-	if err := binary.Write(&buf, binary.LittleEndian, val); err != nil {
+	if size >= DefaultMaxValueSize {
+		return nil
+	}
+
+	byteString := make([]byte, size)
+
+	if _, err := file.ReadAt(byteString, absPos); err != nil {
+		if err == io.EOF {
+			return nil
+		}
 		panic(err)
 	}
 
-	if _, err := f.fileList[filePos].WriteAt(buf.Bytes(), pos); err != nil {
+	return byteString
+}
+
+func (f *fileHashmap) writeByte(absPos int64, value []byte, file *os.File) {
+	if file == nil {
+		panic(fmt.Errorf("file not open"))
+	}
+
+	if value == nil || len(value) == 0 || len(value) >= DefaultMaxValueSize {
+		return
+	}
+
+	if _, err := file.WriteAt(value, absPos); err != nil {
 		panic(err)
 	}
 }
 
-func (f *fileHashmap) readUInt32(absPos int64, filePos uint) uint32 {
-	if f.fileList[filePos] == nil {
-		panic(fmt.Errorf("file %d not open", filePos))
+func (f *fileHashmap) writeUInt32(absPos int64, val uint32, file *os.File) {
+	if file == nil {
+		panic(fmt.Errorf("file not open"))
+	}
+
+	var byteString bytes.Buffer
+	if err := binary.Write(&byteString, binary.LittleEndian, val); err != nil {
+		panic(err)
+	}
+
+	if _, err := file.WriteAt(byteString.Bytes(), absPos); err != nil {
+		panic(err)
+	}
+}
+
+func (f *fileHashmap) readUInt32(absPos int64, file *os.File) uint32 {
+	if file == nil {
+		panic(fmt.Errorf("file not open"))
 	}
 
 	var val uint32
-	bs := make([]byte, 4)
+	byteString := make([]byte, 4)
 
-	if _, err := f.fileList[filePos].ReadAt(bs, absPos); err != nil {
+	if _, err := file.ReadAt(byteString, absPos); err != nil {
 		if err == io.EOF {
 			return math.MaxUint32
 		}
 		panic(err)
 	}
 
-	if err := binary.Read(bytes.NewBuffer(bs), binary.LittleEndian, &val); err != nil {
+	if err := binary.Read(bytes.NewBuffer(byteString), binary.LittleEndian, &val); err != nil {
 		panic(err)
 	}
 
 	return val
 }
 
-func (f *fileHashmap) writeUInt64(pos int64, val uint64, filePos uint) {
-	if f.fileList[filePos] == nil {
-		panic(fmt.Errorf("file %d not open", filePos))
+func (f *fileHashmap) writeUInt64(absPos int64, val uint64, file *os.File) {
+	if file == nil {
+		panic(fmt.Errorf("file not open"))
 	}
 
 	var buf bytes.Buffer
@@ -57,27 +93,27 @@ func (f *fileHashmap) writeUInt64(pos int64, val uint64, filePos uint) {
 		panic(err)
 	}
 
-	if _, err := f.fileList[filePos].WriteAt(buf.Bytes(), pos); err != nil {
+	if _, err := file.WriteAt(buf.Bytes(), absPos); err != nil {
 		panic(err)
 	}
 }
 
-func (f *fileHashmap) readUInt64(pos int64, filePos uint) uint64 {
-	if f.fileList[filePos] == nil {
-		panic(fmt.Errorf("file %d not open", filePos))
+func (f *fileHashmap) readUInt64(absPos int64, file *os.File) uint64 {
+	if file == nil {
+		panic(fmt.Errorf("file not open"))
 	}
 
 	var val uint64
-	bs := make([]byte, 8)
+	byteString := make([]byte, 8)
 
-	if _, err := f.fileList[filePos].ReadAt(bs, pos); err != nil {
+	if _, err := file.ReadAt(byteString, absPos); err != nil {
 		if err == io.EOF {
 			return 0
 		}
 		panic(err)
 	}
 
-	if err := binary.Read(bytes.NewBuffer(bs), binary.LittleEndian, &val); err != nil {
+	if err := binary.Read(bytes.NewBuffer(byteString), binary.LittleEndian, &val); err != nil {
 		panic(err)
 	}
 
